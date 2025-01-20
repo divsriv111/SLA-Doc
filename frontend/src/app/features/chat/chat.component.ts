01a50255ff109b7d4e5620bd7c0365e2f848c3cd
saran <saranbabu777@gmail.com>
2025-01-20 13:37:20 -0500

    ui changes
    Signed-off-by: saran <saranbabu777@gmail.com>

--- frontend/src/app/features/chat/chat.component.ts
+++ frontend/src/app/features/chat/chat.component.ts
@@ -12,21 +12,21 @@ import { TextareaModule } from 'primeng/textarea';
 })
 export class ChatComponent {
 
-  conversation: {text: string, id: number}[] = [];
+  conversation: {text: string, id: number, self: boolean}[] = [];
   form: FormGroup;
 
   constructor(private fb: FormBuilder) {
     /*Dummy text*/
     this.conversation.push({text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
       Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero 
-      asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!`, id: 0});
+      asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!`, id: 0, self: false});
     this.form = this.fb.group({
       text: ['']
     });
   }
 
   send(){
-    this.conversation.push({text: this.form.value.text, id: this.conversation.length});
+    this.conversation.push({text: this.form.value.text, id: this.conversation.length, self: true});
     this.form.reset();
   }
 }
