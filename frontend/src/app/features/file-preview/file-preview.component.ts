60610981c3f3f3588993772fedcfedc398086bfb
saran <saranbabu777@gmail.com>
2025-01-24 14:44:07 -0500

    PDF preview.
    Signed-off-by: saran <saranbabu777@gmail.com>

--- frontend/src/app/features/file-preview/file-preview.component.ts
+++ frontend/src/app/features/file-preview/file-preview.component.ts
@@ -2,10 +2,12 @@ import { Component, Input, SimpleChanges } from '@angular/core';
 import { ChatService } from '../../core/services/chat/chat.service';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
+import { PdfViewerComponent } from '../pdf-viewer/pdf-viewer.component';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-file-preview',
-  imports: [CommonModule, CardModule],
+  imports: [CommonModule, CardModule, PdfViewerComponent, TabsModule],
   templateUrl: './file-preview.component.html',
   styleUrl: './file-preview.component.scss'
 })
